0b86afcb438294eb76c2cef161d4df350cc81599
Raminta90 <raminta.samuleviciene@gmail.com>
2022-06-08 15:43:44 +0300

    first commit

---
+++ src/components/Main.jsx
@@ -0,0 +1,16 @@
+import Contacts from "./Contacts"
+
+const Main = () => {
+  return (
+    <div>
+
+        <Contacts 
+        name={"Raminta"}
+        email={"raminta@gmail.com"}
+        age={18}
+        />
+    </div>
+  )
+}
+
+export default Main
